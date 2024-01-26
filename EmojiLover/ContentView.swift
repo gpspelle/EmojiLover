bace963139035f53a97101cb17b154f234a62a0c
Gabriel Pellegrino da Silva <g.pellegrino.silva@gmail.com>
2024-01-27 00:05:36 +0100

    hello world

--- EmojiLover/ContentView.swift
+++ EmojiLover/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😀, 😃, 😄, 😁
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😄
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .padding()
+            .navigationTitle("Emoji Lovers!")
         }
-        .padding()
     }
 }
 
